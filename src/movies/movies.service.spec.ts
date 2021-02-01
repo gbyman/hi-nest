923048e3e005a143aee50f3988f7fd3a3b4f4468
gbyman <dongchan-2@hanmail.net>
2021-02-02 00:59:09 +0900

    Unit testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { isRgbColor } from 'class-validator';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -15,4 +17,93 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll", () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("getOne", () => {
+    it("should return a movie", () => {
+      service.create({
+        title: "Test Movie",
+        year: 2000,
+        genres : ['test']
+      }); 
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+describe("deleteOne", () => {
+
+  it("deletes a movie", () => {
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000
+    });
+    const beforeDelete = service.getAll().length;
+    service.deleteOne(1);
+    const afterDelete = service.getAll().length;
+    expect(afterDelete).toBeLessThan(beforeDelete);
+  });
+  
+  it("should return a 404", () => {
+    try {
+      service.deleteOne(999);
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(error.message).toEqual('Movie with ID 999 not found.');
+    }
+  });
+});
+
+describe("create", () => {
+  it("should create a movie", () => {
+    const beforeCreate = service.getAll().length;
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000
+    });
+    const afterCreate = service.getAll().length;
+    console.log(beforeCreate, afterCreate);
+    expect(afterCreate).toBeGreaterThan(beforeCreate);
+  });
+});
+
+describe("update", () => {
+  it("should update a movie", () => {
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000
+    });
+    service.update(1, { title: 'Updated Test' });
+    const movie = service.getOne(1);
+    expect(movie.title).toEqual('Updated Test');
+  });
+  it("should throw a NotFoundException", () => {
+    try {
+      service.update(999, {});
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(error.message).toEqual('Movie with ID 999 not found.');
+    }
+  });
+});
+
 });
